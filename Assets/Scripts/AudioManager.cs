d73df16d9d729d8e08763924e4dcbda3cb70eee4
Unknown <edanuramail@gmail.com>
2020-05-11 12:56:03 -0400

    scream sound added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource m_Source;
+    public AudioClip screamClip;
+    void Start()
+    {
+        m_Source = GetComponent<AudioSource>();
+        m_Source.clip = screamClip;
+    }
+
+    // Update is called once per frame
+
+    private bool soundOn = false;
+    void Update()
+    {
+        if (Manager.isScreaming)
+        {
+            m_Source.PlayOneShot(screamClip);
+        }
+        else
+        {
+            m_Source.Stop();
+        }
+    }
+}
